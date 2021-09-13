487215b302cfd1ece734c8b48c64b1ee237d7b7c
Hansel <valkirecesa@gmail.com>
2021-09-13 23:55:09 +0800

    finished decile

--- quartile_decile.js
+++ quartile_decile.js
@@ -3,10 +3,31 @@ const q1_text = document.querySelector('#q1');
 const q2_text = document.querySelector('#q2');
 const q3_text = document.querySelector('#q3');
 
+const d1_text = document.querySelector('#d1');
+const d2_text = document.querySelector('#d2');
+const d3_text = document.querySelector('#d3');
+const d4_text = document.querySelector('#d4');
+const d5_text = document.querySelector('#d5');
+const d6_text = document.querySelector('#d6');
+const d7_text = document.querySelector('#d7');
+const d8_text = document.querySelector('#d8');
+const d9_text = document.querySelector('#d9');
+
 let data; // an array of the data set (sorted)
 let q1;
 let q2;
 let q3;
+
+let d1;
+let d2;
+let d3;
+let d4;
+let d5;
+let d6;
+let d7;
+let d8;
+let d9;
+
 
 function calculate() {
     
@@ -16,14 +37,36 @@ function calculate() {
 
     getQuartilePositions();
 
+    getDecilePositions();
+
     q1_text.innerHTML = getValue(q1);
     q2_text.innerHTML = getValue(q2);
     q3_text.innerHTML = getValue(q3);
+
+    d1_text.innerHTML = getValue(d1);
+    d2_text.innerHTML = getValue(d2);
+    d3_text.innerHTML = getValue(d3);
+    d4_text.innerHTML = getValue(d4);
+    d5_text.innerHTML = getValue(d5);
+    d6_text.innerHTML = getValue(d6);
+    d7_text.innerHTML = getValue(d7);
+    d8_text.innerHTML = getValue(d8);
+    d9_text.innerHTML = getValue(d9);
 }
 
 function reset() {
     data = [];
-    q1 = q2 = q3 = 0;
+    q1 = 0;
+    q2 = 0;
+    q3 = 0;
+    d1 = 0;
+    d2 = 0;
+    d3 = 0;
+    d4 = 0;
+    d5 = 0;
+    d7 = 0;
+    d8 = 0;
+    d9 = 0;
 }
 
 function parseData() {
@@ -58,9 +101,28 @@ function getQuartilePositions() {
 
 }
 
+function getDecilePositions() {
+    
+    function get(k) {
+        console.log(`D${k}: ${k * (data.length + 1) / 10}`);
+        return k * (data.length + 1) / 10;
+    }
+
+    d1 = get(1);
+    d2 = get(2);
+    d3 = get(3);
+    d4 = get(4);
+    d5 = get(5);
+    d6 = get(6);
+    d7 = get(7);
+    d8 = get(8);
+    d9 = get(9);
+}
+
 function isDecimal(x) {
     return x % 1 != 0;
 }
+
 
 function getValue(x) {
     let value = data[x - 1];
